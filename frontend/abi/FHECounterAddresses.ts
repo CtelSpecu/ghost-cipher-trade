ac27e7684b92de86bec9429f09063c877d6da693
ClydeBartholomew <imfcmm540610@outlook.com>
2025-11-06 10:02:00 -0800

    feat: add contract ABI and deployment addresses

---
+++ frontend/abi/FHECounterAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const FHECounterAddresses = { 
+  "11155111": { address: "0x1950e6Deda689ac940d7aa7C9136a252eB8D805d", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
+};
